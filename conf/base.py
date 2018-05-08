15668cbeb0f95b87aaca50388809bfa000bb0abd
Jawish185 <452461684@qq.com>
2018-05-09 06:21:00 +0800

    For juejin stable version

---
+++ conf/base.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+engine = create_engine('mysql://root:pwd@demo@localhost:3306/demo?charset=utf8', encoding="utf8", echo=False)
+BaseDB = declarative_base()
+
+SERVER_HEADER = "http://150.109.33.132:8000"
+
+ERROR_CODE = {
+    "0": "ok",
+    #Users error code
+    "1001": "入参非法",
+    "1002": "用户已注册，请直接登录",
+    
+    "2001": "上传图片不能为空"
+}
